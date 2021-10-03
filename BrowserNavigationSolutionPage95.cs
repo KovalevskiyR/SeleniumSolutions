604d710327cc599d417611619718d7d73c8b35a2
Roman Kovalevskiy <kovalevskyi.roma@gmail.com>
2021-10-03 12:20:58 +0300

    BrowserNavigationSolutionPage95

---
+++ BrowserNavigationSolutionPage95.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace BrowserNavigationSolutionPage95
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {      
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            IWebDriver driver = new ChromeDriver(@"C:\QaLearning", options);
+            driver.Navigate().GoToUrl("https://itechcraft.com/");
+            driver.Navigate().GoToUrl("https://itechcraft.com/about-it-craft/");
+            driver.Navigate().Back();
+            driver.Close();
+        }
+    }
+}

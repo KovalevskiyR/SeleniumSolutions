beed52e3df53012fe127e1050409fc86e7c41111
Roman Kovalevskiy <kovalevskyi.roma@gmail.com>
2021-10-08 17:06:02 +0300

    InteractionWithWebElementPage100 fix string methods

---
+++ InteractionWithWebElementPage100.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace BrowserNavigationSolutionPage95
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            IWebDriver driver = new ChromeDriver(@"C:\QaLearning", options);
+            driver.Navigate().GoToUrl("https://google.com/");
+
+            IWebElement input_search = driver.FindElement(By.CssSelector(".gLFyf.gsfi"));
+            input_search.SendKeys("It Craft");
+            input_search.SendKeys(Keys.Enter);       
+            
+            IWebElement search_results = driver.FindElement(By.XPath("//*[@id='result-stats']"));
+
+            string searchResults = search_results.Text;
+            string result = searchResults.Remove(searchResults.LastIndexOf("("));
+            Console.WriteLine(result);
+
+            IWebElement time_spent_results = driver.FindElement(By.TagName("nobr"));
+            Console.WriteLine(time_spent_results.Text);
+
+            IWebElement second_header = driver.FindElement(By.XPath("//h3[(text()) = 'IT Craft.']"));
+            Console.WriteLine(second_header.Text);
+            
+            driver.Close();
+        }
+    }
+}

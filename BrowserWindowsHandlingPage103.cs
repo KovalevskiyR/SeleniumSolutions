c28ce7110e00d4c75cf3184d5388e0d6ed50c93b
Roman Kovalevskiy <kovalevskyi.roma@gmail.com>
2021-10-12 17:11:27 +0300

    BrowserWindowHandlingPage103

---
+++ BrowserWindowsHandlingPage103.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace BrowserNavigationSolutionPage95
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            IWebDriver driver = new ChromeDriver(@"C:\QaLearning", options);
+            WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 15));
+
+            driver.Navigate().GoToUrl("https://google.com/");
+            string firstTab = driver.CurrentWindowHandle;
+
+            ((IJavaScriptExecutor)driver).ExecuteScript("window.open('https://itechcraft.com/');");
+   
+            driver.SwitchTo().Window(firstTab);
+            
+            IWebElement logo_is_presented = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("img.lnXdpd")));
+
+            driver.Close();
+        }
+    }
+}

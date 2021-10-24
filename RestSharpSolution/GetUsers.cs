a08ef8373812c574896a061d081c950cfb879866
Roman Kovalevskiy <kovalevskyi.roma@gmail.com>
2021-10-24 23:59:20 +0300

    RestSharpSolution/GetUsers refactored methods

--- RestSharpSolution/GetUsers.cs
+++ RestSharpSolution/GetUsers.cs
@@ -13,15 +13,10 @@ namespace ApiAutotestPage130
         {
             var restClient = new RestClient("https://jsonplaceholder.typicode.com/users");
             var restRequest = new RestRequest(Method.GET);
-            restClient.AddDefaultHeader("Accepts", "application/json");
-            restRequest.RequestFormat = DataFormat.Json;
-
             IRestResponse response = restClient.Execute(restRequest);
             var content = response.Content;
-
             var users = JsonConvert.DeserializeObject<List<ListOfUsers>>(content).ToList();
             return users;
-
         }
     }
 }
